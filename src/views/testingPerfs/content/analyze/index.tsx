5283705284e505914956ef3208d59e1a8ffd932c
Francois G <f.gerthoffert@gmail.com>
2021-10-18 11:59:17 -0400

    Update index.tsx

--- src/views/testingPerfs/content/analyze/index.tsx
+++ src/views/testingPerfs/content/analyze/index.tsx
@@ -43,7 +43,7 @@ const Analyze: React.FC<connectedProps> = (props: connectedProps) => {
   if (data !== undefined && data.testingPerfs.data.items.nodes.length > 0) {
     const availableRuns = data.testingPerfs.data.items.nodes
       .slice()
-      .sort((a: any, b: any) => new Date(a.startedAt) - new Date(b.startedAt));
+      .sort((a: any, b: any) => new Date(a.startedAt).getTime() - new Date(b.startedAt).getTime());
     const lastRun = availableRuns[availableRuns.length - 1];
     if (selectedRunId === '') {
       setSelectedRunId(lastRun.id);
